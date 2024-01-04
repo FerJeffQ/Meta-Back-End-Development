6a8528e40692ee4493d9ceb25bc63cb0aee49832
FerJeffQ <ferjeff92@gmail.com>
2024-01-04 12:31:04 -0500

    add request

--- 5. Marco web Django/Semana_2/myproject/myapp/views.py
+++ 5. Marco web Django/Semana_2/myproject/myapp/views.py
@@ -3,5 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def home(response):
-    return HttpResponse("Welcome to little lemon web site!")
+# def home(response):
+#     return HttpResponse("Welcome to little lemon web site!")
+
+def home(request):
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+    
+    response = HttpResponse()
+    response.headers['Age'] = 2
+
+    msg = f"""<br>
+    <br> Path: {path}
+    <br> Address: {address}
+    <br> Scheme: {scheme}
+    <br> Method: {method}
+    <br> User agent: {user_agent}
+    <br> Path info: {path_info}
+    <br> Response header: {response.headers}
+    """
+
+    return HttpResponse(msg, content_type='text/html', charset='utf-8')
