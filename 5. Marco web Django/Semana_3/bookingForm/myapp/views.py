2ff8dc9e13dc2c5e76ab5d4a7d78409ecf2185a6
FerJeffQ <ferjeff92@gmail.com>
2024-01-07 15:29:57 -0500

    practice with forms and databases sqlite

--- 5. Marco web Django/Semana_3/bookingForm/myapp/views.py
+++ 5. Marco web Django/Semana_3/bookingForm/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from myapp.forms import BookingForm
+
+def form_view(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form" : form}
+    return render(request, "booking.html", context)
